93ef51df7ed00d4fd0b45a2d52bc081f2fd555b4
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-06-27 21:26:00 -0300

    refactor: rename folder "services" to "rag" to better reflect purpose

---
+++ rag/vector_store.py
@@ -0,0 +1,27 @@
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+from app.settings import PERSIST_DIRECTORY
+
+_vector_store_instance = None
+
+def load_vector_store():
+    global _vector_store_instance
+
+    if _vector_store_instance is not None:
+        return _vector_store_instance
+
+    try:
+        _vector_store_instance = Chroma(
+            persist_directory=PERSIST_DIRECTORY,
+            embedding_function=OpenAIEmbeddings(),
+        )
+        return _vector_store_instance
+    except Exception as e:
+        print(f'Erro ao recuperar a Vector Store: {e}')
+        return None
+
+def add_documents_to_vector_store(chunks):
+    vector_store = load_vector_store()
+    vector_store.add_documents(chunks)
+    return vector_store
