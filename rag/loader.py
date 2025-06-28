b4a679e48d4bf49bdbad7c5ff76252e966588fbe
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-06-27 21:27:41 -0300

    feat(rag): add function to process PDFs and return chunks

---
+++ rag/loader.py
@@ -0,0 +1,20 @@
+import os
+import tempfile
+from langchain_community.document_loaders import PyPDFLoader
+
+from rag.splitter import split_documents
+
+
+def process_pdf_for_chunks(file):
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
+        temp_file.write(file.read())
+        temp_file_path = temp_file.name
+    
+    loader = PyPDFLoader(temp_file_path)
+    docs = loader.load()
+    
+    os.remove(temp_file_path)
+    
+    chunks = split_documents(docs=docs)
+
+    return chunks
